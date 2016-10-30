2bf231564ebf83caab845f0442241835e817c354
Max <1@porzelt.net>
2016-10-30 22:44:24 +0100

    fix tabs and add bot entities

--- EmeraldAI/Entities/BaseObject.py
+++ EmeraldAI/Entities/BaseObject.py
@@ -3,6 +3,6 @@
 import json
 
 class BaseObject(object):
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-            sort_keys=True, indent=4)
+	def toJSON(self):
+		return json.dumps(self, default=lambda o: o.__dict__,
+			sort_keys=True, indent=4)
--- EmeraldAI/Entities/Word.py
+++ EmeraldAI/Entities/Word.py
@@ -3,9 +3,9 @@
 from EmeraldAI.Entities.BaseObject import BaseObject
 
 class Word(BaseObject):
-  def __init__(self, input):
-    self.Word = input
-    self.Ranking = None
-    self.IsStopWord = False
+	def __init__(self, input):
+		self.Word = input
+		self.Ranking = None
+		self.IsStopWord = False
 
-    self.SynonymList = None
+		self.SynonymList = None
