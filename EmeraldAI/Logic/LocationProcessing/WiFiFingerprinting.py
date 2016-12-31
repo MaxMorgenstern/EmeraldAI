4f4354153caf6b6d1b8152db060f0b1cf8a60769
Max <1@porzelt.net>
2016-12-31 19:13:34 +0100

    Extract Hotspot and extend WIFI Fingerprint + fix old fingerprint test

--- EmeraldAI/Logic/LocationProcessing/WiFiFingerprinting.py
+++ EmeraldAI/Logic/LocationProcessing/WiFiFingerprinting.py
@@ -5,25 +5,8 @@ import plistlib
 import itertools
 from EmeraldAI.Logic.Singleton import Singleton
 from EmeraldAI.Logic.Modules import Global
-
-# TODO: move hotspot into entities - inherit from base object
-class Hotspot(object):
-    BSSID = None
-    SSID = None
-    RSSI = None
-    Noise = None
-    Indicator = None
-    ScanningOS = None
-
-    def __init__(self, BSSID, SSID, Indicator, RSSI=None, Noise=None, ScanningOS=None):
-        self.BSSID = BSSID
-        self.SSID = SSID
-        self.RSSI = RSSI
-        self.Noise = Noise
-        self.Indicator = Indicator
-        if(ScanningOS == None):
-            ScanningOS = Global.OS
-        self.ScanningOS = ScanningOS
+from EmeraldAI.Entities.Hotspot import Hotspot
+from EmeraldAI.Logic.Database.SQlite3 import SQlite3 as db
 
 
 class WiFiFingerprinting(object):
@@ -126,4 +109,56 @@ class WiFiFingerprinting(object):
 
     def __groupSeparator(self, line):
         return line=='\n'
+
+
+
+
+
+
+    def CreateLocation(self, name):
+        return db().Execute("INSERT INTO Fingerprint_Position ('Name') Values ('{0}');".format(name))
+
+    def GetLocationID(self, name):
+        location = db().Fetchall("SELECT * FROM Fingerprint_Position WHERE Name = '{0}'".format(name))
+        #TODO
+        return ""
+
+    def PredictLocation(self):
+        wifiList = self.GetWiFiList()
+        prediction = {}
+
+
+        for wifi in wifiList:
+            query = """SELECT Fingerprint_WiFi.Indicator, ABS({1}-Fingerprint_WiFi.Indicator) as diff, Fingerprint_Position.Name
+            FROM Fingerprint_WiFi, Fingerprint_Position_WiFi, Fingerprint_Position
+            WHERE BSSID = '{0}'
+            AND Fingerprint_WiFi.ID = Fingerprint_Position_WiFi.WiFiID
+            AND Fingerprint_Position_WiFi.PositionID = Fingerprint_Position.ID
+            ORDER BY ABS({1}-Indicator)
+            LIMIT 5"""
+
+            result = db().Fetchall(query.format(wifi['BSSID'], (wifi["RSSI"] - wifi["NOISE"])))
+            for r in result:
+                print r
+                distance = r[1]+1
+                if r[2] in prediction:
+                    prediction[r[2]] += (r[0]/distance)
+                else:
+                    prediction[r[2]] = (r[0]/distance)
+                prediction[r[2]] += 10
+
+        # TODO: maybe ID as well
+        return prediction
+
+
+    def AddWifiToLocation(self, location, wifi):
+        if not isinstance( location, ( int, long ) ):
+            location = self.GetLocationID(location)
+
+        query = "INSERT INTO Fingerprint_WiFi ('BSSID', 'SSID', 'RSSI', 'Noise', 'Indicator') Values ('{0}','{1}','{2}','{3}','{4}');".format(wifi.BSSID, wifi.SSID, wifi.RSSI, wifi.NOISE, wifi.Indicator)
+        wifientry = db().Execute(query)
+
+        query = "INSERT INTO Fingerprint_Position_WiFi ('PositionID', 'WiFiID') VALUES ('{0}','{1}');".format(location, wifientry)
+        db().Execute(query)
+
 
