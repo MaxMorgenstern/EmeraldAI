50732f575ba36e36e7383cf6a6e0e9a824cc3b26
Max <1@porzelt.net>
2017-02-13 23:15:03 +0100

    update trainerFix Trainer issue

--- EmeraldAI/Pipelines/Trainer/Trainer.py
+++ EmeraldAI/Pipelines/Trainer/Trainer.py
@@ -4,6 +4,7 @@ from EmeraldAI.Logic.Singleton import Singleton
 from EmeraldAI.Config.Config import *
 from EmeraldAI.Entities.User import User
 from EmeraldAI.Logic.Trainer.DialogTrainer import *
+from EmeraldAI.Entities.Context import Context
 
 
 class Trainer(object):
@@ -17,12 +18,10 @@ class Trainer(object):
     	if not self.__trainData or not PipelineArgs.TrainConversation:
     		return False
 
-    	if len(PipelineArgs.History) == 0
+    	if len(Context().History) == 0:
     		return False
 
-    	DialogTrainer().TrainSentence(PipelineArgs.History[-1].Response, PipelineArgs.Normalized, PipelineArgs.Language, User().Name):
-
-    	# TODO
+    	DialogTrainer().TrainSentence(Context().History[-1].Response, PipelineArgs.Normalized, PipelineArgs.Language, User().Name)
 
     	return True
 
