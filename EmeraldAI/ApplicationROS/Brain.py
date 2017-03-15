93bf420b97dce7b61e1336f257470c09f1b38b70
Max <1@porzelt.net>
2017-03-15 22:28:11 +0100

    add ros scripts

---
+++ EmeraldAI/ApplicationROS/Brain.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import rospy
+from std_msgs.msg import String
+
+from EmeraldAI.Pipelines.InputProcessing.ProcessInput import ProcessInput
+from EmeraldAI.Pipelines.ScopeAnalyzer.AnalyzeScope import AnalyzeScope
+from EmeraldAI.Pipelines.ResponseProcessing.ProcessResponse import ProcessResponse
+from EmeraldAI.Pipelines.TextToSpeech.TTS import TTS
+from EmeraldAI.Pipelines.Trainer.Trainer import Trainer
+from EmeraldAI.Entities.User import User
+from EmeraldAI.Entities.Context import Context
+from EmeraldAI.Entities.PipelineArgs import PipelineArgs
+
+def RunBrain():
+    rospy.init_node('brain_node', anonymous=True)
+
+    rospy.Subscriber("to_brain", String, callback)
+
+    rospy.spin()
+
+
+def callback(data):
+    dataParts = data.data.split("|")
+    print dataParts
+
+    if dataParts[0] == "CV":
+        ProcessUser(dataParts[1])
+        # ... TODO - initial greeting
+
+    if dataParts[0] == "STT":
+        ProcessSpeech(dataParts[1])
+
+
+
+
+def ProcessUser(cvTag):
+    User().SetUserByCVTag(cvTag)
+
+def ProcessSpeech(data):
+    pipelineArgs = PipelineArgs(data)
+
+    pipelineArgs = ProcessInput().ProcessAsync(pipelineArgs)
+
+    pipelineArgs = AnalyzeScope().Process(pipelineArgs)
+
+    pipelineArgs = ProcessResponse().Process(pipelineArgs)
+
+    pipelineArgs = TTS().Process(pipelineArgs)
+
+    trainerResult = Trainer().Process(pipelineArgs)
+
+    Context().History.append(pipelineArgs)
+
+    print "Pipeline Args", pipelineArgs.toJSON()
+    print "Main User", User().toJSON()
+    print "CV User", CVUserInstance.toJSON()
+    print "Trainer Result: ", trainerResult
+
+
+
+
+if __name__ == "__main__":
+    try:
+        RunBrain()
+    except KeyboardInterrupt:
+        print "End"
---
+++ EmeraldAI/ApplicationROS/STT.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import rospy
+from std_msgs.msg import String
+
+from EmeraldAI.Pipelines.SpeechToText.STT import STT
+
+def RunTTS():
+    pub = rospy.Publisher('to_brain', String, queue_size=10)
+    rospy.init_node('STT_node', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    
+    while True:
+        rate.sleep()
+        data = STT().Process(False)
+        if(data == None):
+            continue
+
+        rospy.loginfo("STT|{0}".format(data))
+        pub.publish("STT|{0}".format(data))
+
+
+if __name__ == "__main__":
+    try:
+        RunTTS()
+    except KeyboardInterrupt:
+        print "End"
