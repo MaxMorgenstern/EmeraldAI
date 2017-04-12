ecfda8f3a423e12f3c768975f5e76b577f461878
Max <1@porzelt.net>
2017-04-12 22:56:27 +0200

    enable full screen and scale gif

--- Android/FaceApp/rosandroid-emeraldai-faceapp/src/main/java/org/EmeraldAI/FaceApp/MainActivity.java
+++ Android/FaceApp/rosandroid-emeraldai-faceapp/src/main/java/org/EmeraldAI/FaceApp/MainActivity.java
@@ -45,27 +45,6 @@ public class MainActivity extends RosActivity {
         // landscape mode
         super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
-
-        /*
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        decorView.setSystemUiVisibility(uiOptions);
-
-        ActionBar actionBar = getActionBar();
-        actionBar.hide();
-        */
-
-
-
-
-
         // maybe try: https://github.com/koral--/android-gif-drawable
 
         // load image and play
@@ -77,8 +56,23 @@ public class MainActivity extends RosActivity {
         catch(IOException ex) {
             return;
         }
-
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
 
     @Override
     protected void init(NodeMainExecutor nodeMainExecutor) {
@@ -90,6 +84,5 @@ public class MainActivity extends RosActivity {
 
         NodeMain node2 = new SimpleSubscriberNode();
         nodeMainExecutor.execute(node2, nodeConfiguration);
-
     }
 }
