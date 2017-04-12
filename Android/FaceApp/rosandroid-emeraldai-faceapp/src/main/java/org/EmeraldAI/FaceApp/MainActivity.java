22ce5b7dc6f7c970e131ca57599dcd67fdf0e032
Max <1@porzelt.net>
2017-04-12 21:31:40 +0200

    update min sdks from 14 to 19

--- Android/FaceApp/rosandroid-emeraldai-faceapp/src/main/java/org/EmeraldAI/FaceApp/MainActivity.java
+++ Android/FaceApp/rosandroid-emeraldai-faceapp/src/main/java/org/EmeraldAI/FaceApp/MainActivity.java
@@ -45,13 +45,23 @@ public class MainActivity extends RosActivity {
         // landscape mode
         super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        // hide action bar and ui
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
+        /*
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         decorView.setSystemUiVisibility(uiOptions);
 
         ActionBar actionBar = getActionBar();
         actionBar.hide();
+        */
+
 
 
 
