de41b99ba3bc8ddb4d58e446ee0ae22ece55e459
Max <1@porzelt.net>
2017-04-20 21:07:23 +0200

    Add dummy gpio class and wrapper class

--- EmeraldAI/Logic/GPIO/EmeraldGPIO.py
+++ EmeraldAI/Logic/GPIO/EmeraldGPIO.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-# TODO: Extend - https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
-
 try:
     import RPi.GPIO as GPIO
 except ImportError:
-    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+    print "Error importing RPi.GPIO! Dummy class is used."
     import gpiodummy as GPIO
 
+from EmeraldAI.Logic.Singleton import Singleton
+
+
 class EmeraldGPIO(object):
-    #__metaclass__ = Singleton
+    __metaclass__ = Singleton
 
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
@@ -34,6 +34,3 @@ class EmeraldGPIO(object):
 
     def cleanup(self):
         GPIO.cleanup()
-
-
-print EmeraldGPIO().input(5)
