71dd33eabc0432bb52f1150b81ee3577d76f83dc
Max <1@porzelt.net>
2017-05-01 14:31:34 +0200

    Fix ModelMonitor

--- EmeraldAI/Logic/ComputerVision/ModelMonitor.py
+++ EmeraldAI/Logic/ComputerVision/ModelMonitor.py
@@ -17,19 +17,23 @@ class ModelMonitor(object):
     	self.__HashFileName = "{0}.npy"
         self.__DatasetBasePath = os.path.join(Global.EmeraldPath, "Data", "ComputerVisionData")
 
-    def Rebuild(self, datasetName, imageSize=None):
-        path = os.path.join(self.__DatasetBasePath, datasetName)
-
+    def CompareHash(self, datasetName, hashValue):
+    	path = os.path.join(self.__DatasetBasePath, datasetName)
         folderHash = Hashing.GetDirHash(path)
+
+        return (folderHash == hashValue)
+
+    def Rebuild(self, datasetName, imageSize=None):
         try:
 	    	storedHash = np.load(os.path.join(self.__DatasetBasePath, self.__HashFileName.format(datasetName))).item()
     	except Exception:
     		storedHash = 0
 
-    	if (folderHash == storedHash):
+    	if (self.CompareHash(datasetName, storedHash)):
     		return
 
     	FileLogger().Info("ModelMonitor: Rebuild {0} Model".format(datasetName))
     	ComputerVision().TrainModel(datasetName, imageSize)
+    	path = os.path.join(self.__DatasetBasePath, datasetName)
         folderHash = Hashing.GetDirHash(path)
     	np.save(os.path.join(self.__DatasetBasePath, self.__HashFileName.format(datasetName)), folderHash)
