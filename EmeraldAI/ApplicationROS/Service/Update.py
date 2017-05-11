32442053ee9aeee7e3b2185430675c7cdb3b1087
Max <1@porzelt.net>
2017-05-12 00:07:49 +0200

    Add Update template

---
+++ EmeraldAI/ApplicationROS/Service/Update.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+# TODO
+
