6142a4b81b8f1cd1ce8772b80441be82851fc10e
Max <1@porzelt.net>
2017-05-11 23:50:19 +0200

    Extend Setup

--- EmeraldAI/ApplicationROS/Service/Setup.py
+++ EmeraldAI/ApplicationROS/Service/Setup.py
@@ -6,4 +6,41 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-# TODO: Setup environment
+import speech_recognition as sr
+from shutil import copyfile
+
+from EmeraldAI.Logic.Modules import Global
+
+
+# Create config file if it does not exist
+configFile = os.path.join(Global.EmeraldPath, "Config", "base.config")
+if not os.path.exists(configFile):
+	exampleConfigFile = os.path.join(Global.EmeraldPath, "Config", "base.config.example")
+	copyfile(exampleConfigFile, configFile)
+
+# Bot
+
+# TODO - Set Name
+
+
+
+# SpeechToText
+
+# TODO - Set Microphone
+
+microphoneDict = {}
+for i, name in enumerate(sr.Microphone().list_microphone_names()):
+	print i, name
+	microphoneDict[i] = name
+
+print "Enter the ID of your primary microphone"
+inputData = int(raw_input("ID: "))
+
+print "You selected:", microphoneDict[inputData]
+
+
+# ComputerVision
+
+# TODO - Set CameraID
+
+
