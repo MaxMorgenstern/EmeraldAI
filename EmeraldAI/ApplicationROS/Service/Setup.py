e4d3bce2f3f27b27a7a7f5b247b63e5a880db1dd
Max <1@porzelt.net>
2017-06-10 13:14:02 +0200

    fupdate setup

--- EmeraldAI/ApplicationROS/Service/Setup.py
+++ EmeraldAI/ApplicationROS/Service/Setup.py
@@ -10,6 +10,7 @@ sys.setdefaultencoding('utf-8')
 import speech_recognition as sr
 from shutil import copyfile
 import ConfigParser
+import cv2
 
 from EmeraldAI.Logic.Modules import Global
 
@@ -20,27 +21,82 @@ cp = ConfigParser.ConfigParser()
 print "Check logging.config"
 logConfigFile = os.path.join(Global.EmeraldPath, "Config", "logging.config")
 if not os.path.exists(logConfigFile):
-	logdir = os.path.join(Global.EmeraldPath, "Data", "Log")
-	print "Copy example logging.config and set log path to '{0}'".format(logdir)
+    logdir = os.path.join(Global.EmeraldPath, "Data", "Log")
+    print "Copy example logging.config and set log path to '{0}'".format(logdir)
 
-	exampleLogConfigFile = os.path.join(Global.EmeraldPath, "Config", "logging.config.example")
-	copyfile(exampleLogConfigFile, logConfigFile)
+    exampleLogConfigFile = os.path.join(Global.EmeraldPath, "Config", "logging.config.example")
+    copyfile(exampleLogConfigFile, logConfigFile)
 
-	cp.read(logConfigFile)
-	cp.set("DEFAULT", "my_log_dir", logdir+"/")
-	with open(logConfigFile, 'wb') as filePointer:
-	    cp.write(filePointer)
+    cp.read(logConfigFile)
+    cp.set("DEFAULT", "my_log_dir", logdir+"/")
+    with open(logConfigFile, 'wb') as filePointer:
+        cp.write(filePointer)
 
 
 # Create config file if it does not exist
 print "Check base.config"
+updateConfig = False
 configFile = os.path.join(Global.EmeraldPath, "Config", "base.config")
 if not os.path.exists(configFile):
-	print "Copy example base.config"
-	exampleConfigFile = os.path.join(Global.EmeraldPath, "Config", "base.config.example")
-	copyfile(exampleConfigFile, configFile)
+    print "Copy example base.config"
+    exampleConfigFile = os.path.join(Global.EmeraldPath, "Config", "base.config.example")
+    copyfile(exampleConfigFile, configFile)
+    updateConfig = True
+else:
+    print "Do you want to update the data in base.config"
+    inputData = raw_input("Y/N: ")
+    if(inputData.lower() == "y"):
+        updateConfig = True
+
+# TODO - which system are we on Brain? CV? TTS?
+
+if(False):
+    cp.read(configFile)
+
+    # Speech To Text
+    # Set Microphone
+    microphoneDict = {}
+    for i, name in enumerate(sr.Microphone().list_microphone_names()):
+        print i, name
+        microphoneDict[i] = name
+
+    if(len(microphoneDict) > 0):
+        print "Enter the ID of your primary microphone"
+        inputData = int(raw_input("ID: "))
+        print "You selected:", microphoneDict[inputData]
+
+        cp.set("SpeechToText", "Microphone", microphoneDict[inputData])
+    else:
+        print "No microphone detected"
 
 
+# ComputerVision
+
+# Set CameraID
+camID = 100
+camera = cv2.VideoCapture(camID)
+
+while True:
+    # TODO timeout
+    if (camera.isOpened() != 0):
+        print "ok"
+        ret, image = camera.read()
+
+        cv2.imshow("Camera {0}".format(camID), image)
+
+        if cv2.waitKey(1) & 0xFF == ord('y'):
+            print "Cam set"
+        if cv2.waitKey(1) & 0xFF == ord('n'):
+            print "Next camera"
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print "Cancel camera selection"
+    else:
+        print "..."
+
+
+
+#with open(configFile, 'wb') as filePointer:
+#    cp.write(filePointer)
 
 
 
@@ -53,22 +109,7 @@ with open(configFile, 'wb') as cfpointer:
 # Bot
 
 
-# SpeechToText
-
-# TODO - Set Microphone
-microphoneDict = {}
-for i, name in enumerate(sr.Microphone().list_microphone_names()):
-	print i, name
-	microphoneDict[i] = name
-
-print "Enter the ID of your primary microphone"
-inputData = int(raw_input("ID: "))
-
-print "You selected:", microphoneDict[inputData]
 
 
-# ComputerVision
-
-# TODO - Set CameraID
 
 
