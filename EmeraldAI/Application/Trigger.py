eda2cb3c8161d3ae217c37de6b1ef1b82931d4df
Max <1@porzelt.net>
2017-08-06 23:00:28 +0200

    fix trigger

--- EmeraldAI/Application/Trigger.py
+++ EmeraldAI/Application/Trigger.py
@@ -7,14 +7,15 @@ sys.path.append(dirname(dirname(dirname(abspath(__file__)))))
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-#import rospy
-#from std_msgs.msg import String
+import rospy
+from std_msgs.msg import String
 
 from EmeraldAI.Logic.Modules import Pid
 from EmeraldAI.Config.Config import *
 from EmeraldAI.Logic.GPIO.GPIOProxy import GPIOProxy
 
 GLOBAL_Publisher = None
+GLOBAL_TriggerName = "default"
 
 def RunTrigger():
     global GLOBAL_Publisher
@@ -22,9 +23,9 @@ def RunTrigger():
     # TODO: Config
     GPIOInputChannel = 3 # Board Pin #3
 
-    GLOBAL_Publisher = rospy.Publisher('to_brain', String, queue_size=10)
-    rospy.init_node('STT_node', anonymous=True)
-    ospy.Rate(10) # 10hz
+    GLOBAL_Publisher = rospy.Publisher('trigger', String, queue_size=10)
+    rospy.init_node('Trigger_node', anonymous=True)
+    rospy.Rate(10) # 10hz
 
     # TODO: Config, which trigger
 
@@ -39,14 +40,19 @@ def RunTrigger():
         time.sleep(1)
 
 def Trigger(callback):
-    global GLOBAL_Publisher
+    global GLOBAL_Publisher, GLOBAL_TriggerName
 
-    triggerData = "TRIGGER|{0}".format(time.time())
+    triggerData = "TRIGGER|{0}|{1}".format(time.time(), GLOBAL_TriggerName)
     rospy.loginfo(triggerData)
     GLOBAL_FaceappPublisher.publish(triggerData)
 
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        for arg in sys.argv:
+            if (arg.lower().startswith("-name")):
+                GLOBAL_TriggerName = int(arg.lower().replace("-name", ""))
+
     if(Pid.HasPid("Trigger")):
         print "Process is already runnung. Bye!"
         sys.exit()
