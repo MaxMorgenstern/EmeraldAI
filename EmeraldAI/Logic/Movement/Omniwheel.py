6399da568adf7c4f2119db13e064e66c021ca14c
Max <1@porzelt.net>
2017-08-27 00:32:32 +0200

    Extend Omniwheel

--- EmeraldAI/Logic/Movement/Omniwheel.py
+++ EmeraldAI/Logic/Movement/Omniwheel.py
@@ -16,6 +16,10 @@ class Omniwheel(object):
                 self.wheel[i] = angle
 
         self.MappingRange = Config().GetInt("Robot.Movement", "MappingRange")
+        self.FrontDegree = 0
+        self.LeftFrontAngleID = 0
+        self.RightFrontAngleID = 1
+        self.MaxMoveAngle = 45
 
 
     def Rotate(self, cw=True, velocity=1):
@@ -29,14 +33,25 @@ class Omniwheel(object):
                 result += "|-{0}".format(self.Map(velocity))
         return result.strip('|')
 
-    def Move(self, angle, velocity=1):
-        result = ""
+
+    def Move(self, angle, velocity=1, straveOnly=False):
+        result = []
+
         for wheel in self.wheel:
             if wheel is None:
                 continue
-            result += "|{0}".format(self.Map(self.CalculatePower(wheel, angle, velocity)))
+            result.append(self.Map(self.CalculatePower(wheel, angle, velocity)))
 
-        return result.strip('|')
+        if (not straveOnly and abs(angle) - self.FrontDegree) < self.MaxMoveAngle:
+            percent = 100 * ((self.MaxMoveAngle * 2) - abs(angle)) / (self.MaxMoveAngle * 2)
+
+            if (angle>0):
+                result[self.RightFrontAngleID] = int(round(result[0] * percent / 100))
+            else:
+                result[self.LeftFrontAngleID] = int(round(result[1] * percent / 100))
+
+        return "|".join(str(x) for x in result)
+
 
     def CalculatePower(self, wheelAngle, angle, velocity=1):
         return -round(velocity * math.cos(math.radians(wheelAngle - angle)), 4)
