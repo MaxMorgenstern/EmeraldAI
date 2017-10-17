4d358d35dd8c952250cebb5ccf4ec6661ca5367d
Max <1@porzelt.net>
2017-10-17 22:37:32 +0200

    add trigger to arm

--- testing/arduino/arm_with_trigger.ino
+++ testing/arduino/arm_with_trigger.ino
@@ -8,6 +8,9 @@ const uint8_t servoPin_Bottom = 3;
 Servo ServoMotor_Top;
 const uint8_t servoPin_Top = 6;
 
+Servo ServoMotor_Trigger;
+const uint8_t servoPin_Trigger = 9;
+
 const uint8_t servoRotationAngel = 10;
 
 
@@ -19,21 +22,17 @@ const uint8_t buttonSW = 4;
 
 void setup()
 {
-    Serial.begin(9600); // Starts the serial communication
-
     pinMode(buttonSW, INPUT_PULLUP);
-
-    pinMode(A5, OUTPUT);
-    pinMode(buttonSW, OUTPUT);
 
     ServoMotor_Bottom.attach(servoPin_Bottom, 400, 2600);
     ServoMotor_Bottom.write(90);
 
     ServoMotor_Top.attach(servoPin_Top, 400, 2600);
     ServoMotor_Top.write(90);
+
+    ServoMotor_Trigger.attach(servoPin_Trigger, 400, 2600);
+    ServoMotor_Trigger.write(100);
 }
-
-
 
 void loop()
 {
@@ -41,10 +40,11 @@ void loop()
 
     ServoMotor_Top.write(map(analogRead(buttonY), 0, 1024, 0, 180));
 
-
-    analogWrite(A5, 255);
     if(digitalRead(buttonSW) == 0)
     {
-        analogWrite(A5, 0);
+        ServoMotor_Trigger.write(135);
+        delay(100);
+        ServoMotor_Trigger.write(100);
     }
+
 }
