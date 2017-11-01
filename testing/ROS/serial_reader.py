8844eb3f9320780ff280f87e3363e580a213a691
Max <1@porzelt.net>
2017-11-01 22:06:00 +0100

    fix include issue and format updates

--- testing/ROS/serial_reader.py
+++ testing/ROS/serial_reader.py
@@ -7,13 +7,14 @@ while True:
 	line = ser.readline().rstrip()
 	if(len(line) <= 1):
 		continue
+
 	data = line.split("|")
 	if(len(data) <= 1):
 		continue
 
 	senstor = data[0]
 	timestamp = data[1]
-	
+
 	Y = data[2]
 	P = data[3]
 	R = data[4]
--- testing/arduino/ArduinoMotionSensorExample/AvrCopter.ino
+++ testing/arduino/ArduinoMotionSensorExample/AvrCopter.ino
@@ -10,16 +10,16 @@
 
 //for identity matrix see inv_mpu documentation how this is calculated; this is overwritten by a config packet
 uint8_t gyro_orientation = 136;
-uint8_t mpu_addr = 0; 
+uint8_t mpu_addr = 0;
 
 int ret;
 void setup() {
-    Fastwire::setup(400,0);
-    Serial.begin(38400);
-    ret = mympu_open(mpu_addr, 200, gyro_orientation);
-    Serial.print("MPU init: "); Serial.println(ret);
-    Serial.print("Free mem: "); Serial.println(freeRam());
-	
+	Fastwire::setup(400,0);
+	Serial.begin(38400);
+	ret = mympu_open(mpu_addr, 200, gyro_orientation);
+	Serial.print("MPU init: "); Serial.println(ret);
+	Serial.print("Free mem: "); Serial.println(freeRam());
+
 }
 
 unsigned int c = 0; //cumulative number of successful MPU/DMP reads
@@ -28,78 +28,85 @@ unsigned int err_c = 0; //cumulative number of MPU/DMP reads that brought corrup
 unsigned int err_o = 0; //cumulative number of MPU/DMP reads that had overflow bit set
 
 void loop() {
-    #if defined MPU9150 || defined MPU9250
-      ret = mympu_update_compass();
-    #endif
-  
-    ret = mympu_update();
+	#if defined MPU9150 || defined MPU9250
+		ret = mympu_update_compass();
+	#endif
 
-    switch (ret) {
-	case 0: c++; break;
-	case 1: np++; return;
-	case 2: err_o++; return;
-	case 3: err_c++; return; 
-	default: 
-		Serial.print("READ ERROR!  ");
-		Serial.println(ret);
-		return;
-    }
+	ret = mympu_update();
 
-    if (!(c%25)) {
-      #if defined MPU6050
-        Serial.print("MPU6050|"); 
-      #endif
+	switch (ret) {
+		case 0: c++; break;
+		case 1: np++; return;
+		case 2: err_o++; return;
+		case 3: err_c++; return;
+		default:
+			Serial.print("READ ERROR!  ");
+			Serial.println(ret);
+			return;
+	}
 
-      #if defined MPU9150 
-        Serial.print("MPU9150|"); 
-      #endif
+	// Every 25 Packages go through this
+	if (!(c%25)) {
 
-      #if defined MPU6500
-        Serial.print("MPU6500|"); 
-      #endif
-      
-      #if defined MPU9250
-        Serial.print("MPU9250|"); 
-      #endif
-      
-      Serial.print(np); 
-      Serial.print("|"); 
-      
-      Serial.print(mympu.ypr[0]);
-      Serial.print("|"); 
-      Serial.print(mympu.ypr[1]);
-      Serial.print("|"); 
-      Serial.print(mympu.ypr[2]);
-      Serial.print("|"); 
-      
-      Serial.print(mympu.gyro[0]);
-      Serial.print("|"); 
-      Serial.print(mympu.gyro[1]);
-      Serial.print("|"); 
-      Serial.print(mympu.gyro[2]);
-      Serial.print("|"); 
-      
-      
-      Serial.print(mympu.accel[0]);
-      Serial.print("|"); 
-      Serial.print(mympu.accel[1]);
-      Serial.print("|"); 
-      Serial.print(mympu.accel[2]);
-      Serial.print("|"); 
+		// IMU Type
+		#if defined MPU6050
+			Serial.print("MPU6050|");
+		#endif
 
-      #if defined MPU9150 || defined MPU9250
-        Serial.print(mympu.comp[0]);
-        Serial.print("|"); 
-        Serial.print(mympu.comp[1]);
-        Serial.print("|"); 
-        Serial.print(mympu.comp[2]);
-        Serial.print("|"); 
-      #endif
-      #if !defined MPU9150 && !defined MPU9250
-        Serial.print("0|0|0");
-      #endif
-      
-      Serial.println(""); 
-    }
+		#if defined MPU9150
+			Serial.print("MPU9150|");
+		#endif
+
+		#if defined MPU6500
+			Serial.print("MPU6500|");
+		#endif
+
+		#if defined MPU9250
+			Serial.print("MPU9250|");
+		#endif
+
+		// No Package Count
+		Serial.print(np);
+		Serial.print("|");
+
+		// Yaw, Pitch, Roll
+		Serial.print(mympu.ypr[0]);
+		Serial.print("|");
+		Serial.print(mympu.ypr[1]);
+		Serial.print("|");
+		Serial.print(mympu.ypr[2]);
+		Serial.print("|");
+
+		// Gyroscope
+		Serial.print(mympu.gyro[0]);
+		Serial.print("|");
+		Serial.print(mympu.gyro[1]);
+		Serial.print("|");
+		Serial.print(mympu.gyro[2]);
+		Serial.print("|");
+
+		// Accelerometer
+		Serial.print(mympu.accel[0]);
+		Serial.print("|");
+		Serial.print(mympu.accel[1]);
+		Serial.print("|");
+		Serial.print(mympu.accel[2]);
+		Serial.print("|");
+
+		// Magnetometer / Compas
+		#if defined MPU9150 || defined MPU9250
+			Serial.print(mympu.comp[0]);
+			Serial.print("|");
+			Serial.print(mympu.comp[1]);
+			Serial.print("|");
+			Serial.print(mympu.comp[2]);
+			Serial.print("|");
+		#endif
+		#if !defined MPU9150 && !defined MPU9250
+			Serial.print("0|0|0");
+		#endif
+
+		Serial.println("");
+	}
 }
 
