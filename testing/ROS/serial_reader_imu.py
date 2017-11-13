2a2ad2fdac19950707c07f099a23f8a217ca7f77
Max <1@porzelt.net>
2017-11-13 22:30:21 +0100

    add tf2 launch and port imu reader to tf2

--- testing/ROS/serial_reader_imu.py
+++ testing/ROS/serial_reader_imu.py
@@ -6,9 +6,12 @@ import rospy
 import os
 import sys
 import math
-import tf
+
+import tf2_ros as tf
+import tf_conversions as tf_conv
 
 from sensor_msgs.msg import Imu
+from geometry_msgs.msg import TransformStamped
 
 
 def RadianToDegree(rad):
@@ -17,6 +20,23 @@ def RadianToDegree(rad):
 def DegreeToRadian(deg):
     return (deg * 71) / 4068.0
 
+def SendTF2Transform(transformBroadcaster, translation, rotation, time, child_frame_id, frame_id):
+    t = TransformStamped()
+ 
+    t.header.stamp = time
+    t.header.frame_id = frame_id
+    t.child_frame_id = child_frame_id
+    t.transform.translation.x = translation[0]
+    t.transform.translation.y = translation[1]
+    t.transform.translation.z = translation[2]
+    #t.transform.rotation = rotation
+
+    t.transform.rotation.x = rotation[0]
+    t.transform.rotation.y = rotation[1]
+    t.transform.rotation.z = rotation[2]
+    t.transform.rotation.w = rotation[3]
+
+    transformBroadcaster.sendTransform(t)
 
 if __name__=="__main__":
 
@@ -28,7 +48,6 @@ if __name__=="__main__":
 	transformBroadcaster = tf.TransformBroadcaster()
 
 	port_name = "/dev/ttyUSB0"
-	#baud = 57600 # 230400
 	baud = 230400
 
 	if len(sys.argv) >= 2 :
@@ -80,8 +99,8 @@ if __name__=="__main__":
 
 
 		imuMessage.header.stamp = rospy.Time.now()
-		quaternion = tf.transformations.quaternion_from_euler(Y, P, R, 'rzyx')
-		#quaternion = tf.transformations.quaternion_from_euler(R, P, Y)
+		quaternion = tf_conv.transformations.quaternion_from_euler(Y, P, R, 'rzyx')
+		#quaternion = tf_conv.transformations.quaternion_from_euler(R, P, Y)
 		
 		imuMessage.orientation.x = quaternion[0]
 		imuMessage.orientation.y = quaternion[1]
@@ -104,7 +123,13 @@ if __name__=="__main__":
 		imuPub.publish(imuMessage)
 
 		# translation (x,y,z), rotation(yaw-pitch-roll (ZYX) ), time, child, parent
-		transformBroadcaster.sendTransform((0, 0, 1),
+		#transformBroadcaster.sendTransform((0, 0, 1),
+		#	quaternion,
+		#	rospy.Time.now(),
+		#	imuMessage.header.frame_id,
+		#	imuParentFrameID)
+		SendTF2Transform(transformBroadcaster,
+			(0, 0, 0.5),
 			quaternion,
 			rospy.Time.now(),
 			imuMessage.header.frame_id,
