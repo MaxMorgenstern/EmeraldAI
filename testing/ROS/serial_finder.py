82a557c7731935c9eba5e7bae9b41db93f7e1ecb
Max <1@porzelt.net>
2017-11-15 23:00:31 +0100

    extend finder

--- testing/ROS/serial_finder.py
+++ testing/ROS/serial_finder.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import subprocess
+import itertools
+
 
 #command = "ll /sys/class/tty/ttyUSB*"
 #command = "ls -al /sys/class/tty/ttyUSB*"
@@ -10,7 +12,25 @@ proc = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
 (out, err) = proc.communicate()
 
 if len(out) < 2:
-	print "no data"
+    print "no data"
 
-print "program output:", out
+# tmp
+out = """/sys/class/tty/ttyUSB0 
+/sys/class/tty/ttyUSB1
+ """
+
+def groupSeparator(line):
+    return line=='\n'
+
+def split(data):
+    lines = []
+    for key, group in itertools.groupby(data, groupSeparator):
+        line = ''.join(str(e) for e in group)
+        line = line.strip()
+        if (len(line) > 1):
+            lines.append(line)
+    return lines
+
+
+print "program output: ", split(out)
 
