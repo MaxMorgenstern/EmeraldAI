553066de17e6f47ea192dfa961e7439fab7bc673
Max <1@porzelt.net>
2017-11-19 20:57:15 +0100

    extend finder and interrupt test

--- testing/ROS/serial_finder.py
+++ testing/ROS/serial_finder.py
@@ -3,10 +3,9 @@ import os
 import subprocess
 import itertools
 
-
-#command = "ll /sys/class/tty/ttyUSB*"
-#command = "ls -al /sys/class/tty/ttyUSB*"
 command = """ls -al /sys/class/tty/ttyUSB* | grep -o "/sys/class/tty/ttyUSB.*"| sed 's/ -> .*//'"""
+replace = "/sys/class/tty"
+replaceWith = "/dev"
 
 proc = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
 (out, err) = proc.communicate()
@@ -15,9 +14,9 @@ if len(out) < 2:
     print "no data"
 
 # tmp
-out = """/sys/class/tty/ttyUSB0 
-/sys/class/tty/ttyUSB1
- """
+#out = """/sys/class/tty/ttyUSB0 
+#/sys/class/tty/ttyUSB1
+# """
 
 def groupSeparator(line):
     return line=='\n'
@@ -28,7 +27,7 @@ def split(data):
         line = ''.join(str(e) for e in group)
         line = line.strip()
         if (len(line) > 1):
-            lines.append(line)
+            lines.append(line.replace(replace, replaceWith))
     return lines
 
 
--- testing/arduino/interrupttest.ino
+++ testing/arduino/interrupttest.ino
@@ -1,31 +1,55 @@
-const byte interruptPin = 2;
-int interruptCount = 0;
-int stepsPerRevelation = 20;
-int distanceInMMPerRevelation = 220;
+const byte interrupt1Pin = 2;
+int interrupt1Count = 0;
+
+const byte interrupt2Pin = 3;
+int interrupt2Count = 0;
+
+const int stepsPerRevelation = 20;
+const int distanceInMMPerRevelation = 220;
 
 void setup() {
-    pinMode(interruptPin, INPUT);
-    attachInterrupt(digitalPinToInterrupt(interruptPin), trigger, RISING);
+    pinMode(interrupt1Pin, INPUT);
+    pinMode(interrupt2Pin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(interrupt1Pin), countRotation1, RISING);
+    attachInterrupt(digitalPinToInterrupt(interrupt2Pin), countRotation2, RISING);
     Serial.begin(230400);
 }
 
-void loop() {
-    int wheelID = 1;
+void sendData(int id)
+{
     Serial.print("Wheel");
     Serial.print("|");
-    Serial.print(wheelID);
+    Serial.print(id);
     Serial.print("|");
-    Serial.print(interruptCount);
+    int count = 0;
+    if(id == 1)
+    {
+        count = interrupt1Count;
+    }
+    if(id == 2)
+    {
+        count = interrupt2Count;
+    }
+    Serial.print(count);
     Serial.print("|");
     Serial.print(stepsPerRevelation);
     Serial.print("|");
     Serial.print(distanceInMMPerRevelation);
     Serial.print("|");
-    Serial.print(distanceInMMPerRevelation/stepsPerRevelation*interruptCount);
+    Serial.print(distanceInMMPerRevelation/stepsPerRevelation*count);
     Serial.println("");
-    delay(200);
 }
 
-void trigger() {
-    interruptCount++;
+void countRotation1() {
+    interrupt1Count++;
+}
+
+void countRotation2() {
+    interrupt2Count++;
+}
+
+void loop() {
+    sendData(1);
+    sendData(2);
+    delay(200);
 }
