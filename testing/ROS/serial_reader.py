fe2bf275157aef7cd0a76cd505ee60efb0a6149c
Max <1@porzelt.net>
2017-11-29 20:47:01 +0100

    slightly extend base reader

--- testing/ROS/serial_reader.py
+++ testing/ROS/serial_reader.py
@@ -8,87 +8,106 @@ import sys
 import time
 import subprocess
 
+
 class SerialHelper():
-	SerialPointer = None
-	ReadTimestamp = None
-	InitialTimestampDelay = 10
+    SerialPointer = None
+    ReadTimestamp = None
+    InitialTimestampDelay = 10
 
-	def __init__ (self, port_name, baud, timeout=1):
-		self._port_name = port_name
-		self._baud = baud
-		self._timeout = timeout
+    def __init__(self, port_name, baud, timeout=1):
+        self._port_name = port_name
+        self._baud = baud
+        self._timeout = timeout
 
-		self.SerialPointer = serial.Serial(port_name, baud, timeout=timeout)
-		self.ReadTimestamp = time.time() + self.InitialTimestampDelay
+        self.SerialPointer = serial.Serial(port_name, baud, timeout=timeout)
+        self.ReadTimestamp = time.time() + self.InitialTimestampDelay
 
-	def Read(self):
-		# if nothing to read, sleep for 10 milliseconds and check timeout
-		while self.SerialPointer.inWaiting() == 0:
-			time.sleep(0.01)
-			if(self.ReadTimestamp + 5 < time.time()):
-				print "reconnect..."
-				self.SerialPointer.close()
-				time.sleep(1)
-				self.SerialPointer = serial.Serial(self._port_name, self._baud, timeout=self._timeout)
-				self.ReadTimestamp = time.time() + self.InitialTimestampDelay
-		return self.SerialPointer.readline().rstrip()
+    def Read(self):
+        # if nothing to read, sleep for 10 milliseconds and check timeout
+        while self.SerialPointer.inWaiting() == 0:
+            time.sleep(0.01)
+            if(self.ReadTimestamp + 5 < time.time()):
+                print "reconnect..."
+                self.SerialPointer.close()
+                time.sleep(1)
+                self.SerialPointer = serial.Serial(self._port_name, self._baud, timeout=self._timeout)
+                self.ReadTimestamp = time.time() + self.InitialTimestampDelay
+        return self.SerialPointer.readline().rstrip()
 
-	def ValidateAndProcess(self, line, length):
-		if(len(line) <= 1):
-			return None
+    def Validate(self, line):
+        if(len(line) <= 1):
+            return None
 
-		data = line.split("|")
-		if(len(data) <= 1):
-			return None
+        data = line.split("|")
+        if(len(data) <= 1):
+            return None
 
-		if(len(data) != length):
-			return None
-
-		self.ReadTimestamp = time.time()
-		return data
+        self.ReadTimestamp = time.time()
+        return data
 
 
 class SerialFinder():
-	_command = """ls -al /sys/class/tty/ttyUSB* | grep -o "/sys/class/tty/ttyUSB.*"| sed 's/ -> .*//'"""
+    _command = """ls -al /sys/class/tty/ttyUSB* | grep -o "/sys/class/tty/ttyUSB.*"| sed 's/ -> .*//'"""
 
-	def Find(self):
+    def Find(self):
 
-		proc = subprocess.Popen([self._command], stdout=subprocess.PIPE, shell=True)
-		(out, err) = proc.communicate()
+        proc = subprocess.Popen(
+            [self._command], stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
 
-		if len(out) < 2:
-			return None
-		
-		print out
+        if len(out) < 2:
+            return None
+
+        print out
 
 
+class SerialWheelToOdometry():
 
-if __name__=="__main__":
+    def Process(self, data):
+        return
 
-	uid = str(os.getpid())
-	rospy.init_node("serial_reader_{0}".format(uid))
-	rospy.loginfo("ROS Serial Python Node '{0}'".format(uid))
 
-	port_name = "/dev/ttyUSB0"
-	baud = 230400
+if __name__ == "__main__":
 
-	if len(sys.argv) >= 2 :
-		port_name  = sys.argv[1]
+    uid = str(os.getpid())
+    rospy.init_node("serial_reader_{0}".format(uid))
+    rospy.loginfo("ROS Serial Python Node '{0}'".format(uid))
 
-	if len(sys.argv) >= 3 :
-		baud  = int(sys.argv[2])
+    port_name = "/dev/ttyUSB0"
+    baud = 230400
 
-	sh = SerialHelper(port_name, baud, timeout=1)
-	
-	while True:
-		line = sh.Read()
-		
-		data = sh.ValidateAndProcess(line, 14)
+    if len(sys.argv) >= 2:
+        port_name = sys.argv[1]
 
-		if(data == None):
-			continue
+    if len(sys.argv) >= 3:
+        baud = int(sys.argv[2])
 
-		print data
-		
+    sh = SerialHelper(port_name, baud, timeout=1)
+
+    while True:
+        line = sh.Read()
+
+        data = sh.Validate(line)
+
+        if(data == None):
+            continue
+
+        # check for type - data[0]
+        # ultrasonic - length: 10 -- multiple messageTypes depending on MPU
+        # wheel - length 10
+        # radar - length: 4
+
+        if(len(data) != 14):
+            continue
+
+        # if(data[0].lower() == "wheel")
+        #             SerialWheelToOdometry().Process(data)
+        # if(data[0].lower() == "MPU6050".lower()) # MPU9150 - MPU6500 - MPU9250
+        #             SerialImuToImu().Process(data)
+        # if(data[0].lower() == "ultrasonic")
+        #             SerialRadarToRange().Process(data)
+
+        print data
+
 
 print "Bye!"
