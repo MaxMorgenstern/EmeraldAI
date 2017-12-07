ff886db78e97f642bcc48eb9df7f23b7f1afc9a5
Max <1@porzelt.net>
2017-12-08 00:16:51 +0100

    add robot_localization

--- EmeraldAI/Logic/ROS/Serial/SerialImuToImu.py
+++ EmeraldAI/Logic/ROS/Serial/SerialImuToImu.py
@@ -21,14 +21,14 @@ class SerialImuToImu():
         rospy.init_node("serial_reader_imu_{0}".format(uid))
         rospy.loginfo("ROS Serial Python Node '{0}'".format(uid))
 
-        self.__imuPublisher = rospy.Publisher('/imu', Imu, queue_size=10)
+        self.__imuPublisher = rospy.Publisher('/imu_data', Imu, queue_size=10)
 
     def Validate(self, data):
         if(data is None):
             return False
         if(len(data) != self.Length):
             return False
-        if(data[0].lower() in "MPU6050 MPU6500 MPU9150MPU9250".lower()):
+        if(data[0].lower() in "MPU6050 MPU6500 MPU9150 MPU9250".lower()):
             return True
         return False
 
@@ -45,7 +45,6 @@ class SerialImuToImu():
         AccelY = float(data[9])
         AccelZ = float(data[10])
 
-        # TODO - currently not in use
         MagnetX = float(data[11])
         MagnetY = float(data[12])
         MagnetZ = float(data[13])
@@ -55,18 +54,18 @@ class SerialImuToImu():
         imuMessage.header.frame_id = imuFrameID
         imuMessage.header.stamp = rospy.Time.now()
         quaternion = tf_conv.transformations.quaternion_from_euler(Y, P, R, 'rzyx')
-
+        
         imuMessage.orientation.x = quaternion[0]
         imuMessage.orientation.y = quaternion[1]
         imuMessage.orientation.z = quaternion[2]
         imuMessage.orientation.w = quaternion[3]
         imuMessage.orientation_covariance = [0.0025, 0, 0, 0, 0.0025, 0, 0, 0, 0.0025]
-
+        
         imuMessage.linear_acceleration.x = AccelX
         imuMessage.linear_acceleration.y = AccelY
         imuMessage.linear_acceleration.z = AccelZ
         imuMessage.linear_acceleration_covariance = [0.02, 0, 0, 0, 0.02, 0, 0, 0, 0.02]
-
+        
         imuMessage.angular_velocity.x = GyroX
         imuMessage.angular_velocity.y = GyroY
         imuMessage.angular_velocity.z = GyroZ
@@ -76,10 +75,12 @@ class SerialImuToImu():
         rospy.loginfo(imuMessage)
 
         self.__imuPublisher.publish(imuMessage)
-
+        
+        """
         TFHelper.SendTF2Transform(
             translation,
             quaternion,
             imuMessage.header.stamp,
             imuFrameID,
             imuParentFrameID)
+        """
