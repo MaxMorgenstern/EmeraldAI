c1968b9e5ab44573d12d6579520b7a1ffdecb76f
Max <1@porzelt.net>
2017-12-09 23:34:37 +0100

    add multiprocessing to serial

--- testing/ROS/serial_multi_converter.py
+++ testing/ROS/serial_multi_converter.py
@@ -5,6 +5,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+import multiprocessing
+import time
+
 from EmeraldAI.Logic.ROS.Serial.SerialFinder import SerialFinder
 from EmeraldAI.Logic.ROS.Serial.SerialReader import SerialReader
 from EmeraldAI.Logic.ROS.Serial.SerialWheelToOdometry import SerialWheelToOdometry
@@ -12,19 +15,8 @@ from EmeraldAI.Logic.ROS.Serial.SerialRadarToRange import SerialRadarToRange
 from EmeraldAI.Logic.ROS.Serial.SerialImuToImu import SerialImuToImu
 
 
-if __name__=="__main__":
-
-    portName = "/dev/ttyUSB0"
-    baud = 230400
-
-    finderResult = SerialFinder().Find()
-    if(not finderResult or len(finderResult) == 0):
-        # TODO
-        exit()
-
-    #portName = finderResult[0]
-
-    reader = SerialReader(portName, baud)
+def Processing(port, baud):
+    reader = SerialReader(port, baud)
 
     wheelToOdom = SerialWheelToOdometry(318, 100, 20)
     imuToImu = SerialImuToImu()
@@ -46,5 +38,37 @@ if __name__=="__main__":
         if(radarToRange.Validate(data)):
             radarToRange.Process(data)
             continue
+
+
+
+if __name__=="__main__":
+
+    portName = "/dev/ttyUSB0"
+    baud = 230400
+
+    processList = {}
+
+    while True:
+        finderResult = SerialFinder().Find()
+        
+        if(not finderResult or len(finderResult) == 0):
+            time.sleep(10)
+            continue
+
+        for port in finderResult:
+            if port in processList:
+                continue
+            else:
+                print "Launching Process for", portName
+                process = multiprocessing.Process(name=portName, target=Processing, args=(portName,baud,))
+                process.start()
+                processList[port] = process
+
+        for process in processList:
+            if process not in finderResult:
+                # todo
+                del processList[process]
+
+        time.sleep(30)
 
 print "Bye!"
