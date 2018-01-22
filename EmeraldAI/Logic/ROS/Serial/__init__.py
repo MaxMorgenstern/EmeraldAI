36964edf16a0df75a924e8227294c64696344bbf
Max <1@porzelt.net>
2018-01-22 23:04:51 +0100

    fix init

--- EmeraldAI/Logic/ROS/Serial/__init__.py
+++ EmeraldAI/Logic/ROS/Serial/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["SerialConnector", "SerialFinder", "SerialImuToImu", "SerialRadarToRange", "SerialWheelToOdometry", "TwistToSerialWheel"]
+__all__ = ["SerialConnector", "SerialFinder", "SerialImuToImu", "SerialRadarToRangeAndLaser", "SerialWheelToOdometry", "TwistToSerialWheel"]
