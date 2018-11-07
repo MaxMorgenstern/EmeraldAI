5790d9a9df26ee6aaa85f4ba1d3e7f1ba46b0335
Max <1@porzelt.net>
2018-11-07 22:36:48 +0100

    add installer

---
+++ EmeraldAI/Application V2/ExecutionScripts/Service/Install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Install requirements via pip"
+#pip install --user -r install_requirements.txt
+
+echo "Update apt-get"
+#apt-get update
+
+echo "Install requirements via apt-get"
+#apt-get install python-opencv -y
+#apt-get install pyaudio -y
+#apt-get install pocketsphinx -y
+#apt-get install portaudio19-dev -y
+#apt-get install ros-kinetic-rosserial-python -y
+
+echo "Create database"
+
+echo "Create config files"
+
+echo "Install requirements via pip"
+
+echo "Trigger Setup"
+
+echo "Trigger Conversation Setup"
+
+echo "Build CV Model"
+
