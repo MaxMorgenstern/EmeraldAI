87896541a40356c9761ce8e409ed7eb0762b5bf4
Max <1@porzelt.net>
2018-11-10 22:34:26 +0100

    extend install script

--- EmeraldAI/Application V2/ExecutionScripts/Service/Install.sh
+++ EmeraldAI/Application V2/ExecutionScripts/Service/Install.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+directory=`dirname $0`
+
 echo "Install requirements via pip"
-#pip install --user -r install_requirements.txt
+pip install --user -r install_requirements.txt
 
 echo "Update apt-get"
-#apt-get update
+apt-get update
 
 echo "Install requirements via apt-get"
-#apt-get install python-opencv -y
-#apt-get install pyaudio -y
-#apt-get install pocketsphinx -y
-#apt-get install portaudio19-dev -y
-#apt-get install ros-kinetic-rosserial-python -y
-
-echo "Create database"
-
-echo "Create config files"
-
-echo "Install requirements via pip"
+apt-get install python-opencv -y
+apt-get install pyaudio -y
+apt-get install pocketsphinx -y
+apt-get install portaudio19-dev -y
+apt-get install ros-kinetic-rosserial-python -y
 
 echo "Trigger Setup"
+python $directory/../../Service/Setup.py
 
 echo "Trigger Conversation Setup"
+python $directory/../../Service/Setup_Conversation.py
 
 echo "Build CV Model"
+python $directory/../../Service/CVModelRebuilder.py
+
+echo "Done!"
 
