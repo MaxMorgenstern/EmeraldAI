52ab27b3f99300b7b97e7d244a86d381c6cc5585
Max <1@porzelt.net>
2019-01-08 11:02:16 +0100

    sqlite3 test

--- EmeraldAI/Logic/Database/SQlite3.py
+++ EmeraldAI/Logic/Database/SQlite3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sqlite3 as lite
+#import sqlite3 as lite
 from cachetools import cached
 from EmeraldAI.Logic.Singleton import Singleton
 from EmeraldAI.Logic.Modules import Global
@@ -16,15 +16,21 @@ class SQlite3(object):
         self.__Database = self.GetDB(Config().Get("Database", "SQliteDatabase"))
 
     def GetDB(self, database):
-        con = lite.connect(os.path.join(Global.EmeraldPath, "Data", "SqliteDB", database.rstrip(".sqlite") + ".sqlite"), check_same_thread=False)
-        con.text_factory = str
-        return con
+        #con = lite.connect(os.path.join(Global.EmeraldPath, "Data", "SqliteDB", database.rstrip(".sqlite") + ".sqlite"), check_same_thread=False)
+        #con.text_factory = str
+        #return con
+        return Worker(os.path.join(Global.EmeraldPath, "Data", "SqliteDB", database.rstrip(".sqlite") + ".sqlite"))
 
     @cached(cache={})
     def Execute(self, sql, args=None):
         return self.ExecuteDB(self.__Database, sql, args)
 
     def ExecuteDB(self, db, sql, args=None):
+        db.execute(sql, args)
+
+        return db.getLastrowid()
+
+        """
         try:
             cur = db.cursor()
             if args:
@@ -40,6 +46,7 @@ class SQlite3(object):
         except lite.OperationalError as e:
             FileLogger().Error("SQlite3 Line 40: OperationError: {0}".format(e))
             return None
+        """
 
     @cached(cache={})
     def Fetchall(self, sql, args=None):
@@ -49,6 +56,10 @@ class SQlite3(object):
         return self.FetchallDB(self.__Database, sql, args)
 
     def FetchallDB(self, db, sql, args=None):
+        return db.execute(sql, args)
+
+
+        """
         try:
             cur = db.cursor()
             if args:
@@ -60,11 +71,138 @@ class SQlite3(object):
             FileLogger().Error("SQlite3 Line 59: {0}".format(e))
             return []
         return rows
+        """
 
 
     def Disconnect(self):
         self.DisconnectDB(self.__Database)
 
     def DisconnectDB(self, db):
-        db.commit()
         db.close()
+
+
+###############################################################################
+
+# Copyright (c) 2014 Palantir Technologies
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""Thread safe sqlite3 interface."""
+
+#__author__ = "Shawn Lee"
+#__email__ = "dashawn@gmail.com"
+#__license__ = "MIT"
+
+import sqlite3
+import threading
+import uuid
+
+try:
+    import queue as Queue  # module re-named in Python 3
+except ImportError:
+    import Queue
+
+
+class Worker(threading.Thread):
+    def __init__(self, file_name, max_queue_size=100):
+        threading.Thread.__init__(self, name=__name__)
+        self.daemon = True
+        self._sqlite3_conn = sqlite3.connect(
+            file_name, check_same_thread=False,
+            detect_types=sqlite3.PARSE_DECLTYPES)
+        self._sqlite3_conn.text_factory = str
+        self._sqlite3_cursor = self._sqlite3_conn.cursor()
+        self._sql_queue = Queue.Queue(maxsize=max_queue_size)
+        self._results = {}
+        self._max_queue_size = max_queue_size
+        self._select_events = {}
+        self._close_event = threading.Event()
+        self._close_lock = threading.Lock()
+        self.start()
+
+    def run(self):
+        execute_count = 0
+        for token, query, values in iter(self._sql_queue.get, None):
+            if query:
+                self._run_query(token, query, values)
+                execute_count += 1
+                if (self._sql_queue.empty() or
+                        execute_count == self._max_queue_size):
+                    self._sqlite3_conn.commit()
+                    execute_count = 0
+
+            if self._close_event.is_set() and self._sql_queue.empty():
+                self._sqlite3_conn.commit()
+                self._sqlite3_conn.close()
+                return
+
+    def _run_query(self, token, query, values):
+        if query.lower().strip().startswith("select"):
+            try:
+                self._sqlite3_cursor.execute(query, values)
+                self._results[token] = self._sqlite3_cursor.fetchall()
+            except sqlite3.Error as err:
+                self._results[token] = (
+                    "Query returned error: %s: %s: %s" % (query, values, err))
+            finally:
+                self._select_events.setdefault(token, threading.Event())
+                self._select_events[token].set()
+        else:
+            try:
+                self._sqlite3_cursor.execute(query, values)
+            except sqlite3.Error as err:
+                # TODO
+                print err
+
+    def close(self):
+        with self._close_lock:
+            if not self.is_alive():
+                return "Already Closed"
+            self._close_event.set()
+            self._sql_queue.put(("", "", ""), timeout=5)
+            self.join()
+
+    @property
+    def queue_size(self):
+        return self._sql_queue.qsize()
+
+    def _query_results(self, token):
+        try:
+            # Wait until the select query has executed
+            self._select_events.setdefault(token, threading.Event())
+            self._select_events[token].wait()
+            return self._results[token]
+        finally:
+            self._select_events[token].clear()
+            del self._results[token]
+            del self._select_events[token]
+
+    def execute(self, query, values=None):
+        if self._close_event.is_set():
+            return "Close Called"
+        values = values or []
+        token = str(uuid.uuid4())
+        self._sql_queue.put((token, query, values), timeout=5)
+
+        if query.lower().strip().startswith("select"):
+            return self._query_results(token)
+
+    def getLastrowid(self):
+        return self._sqlite3_cursor.lastrowid
+
