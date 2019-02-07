16da5ec4cd99472636cb9f85f576d796a6db2f3e
Max <1@porzelt.net>
2019-02-07 21:53:25 +0100

    install improvements

--- EmeraldAI/Application/ExecutionScripts/Service/Install.sh
+++ EmeraldAI/Application/ExecutionScripts/Service/Install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 directory=`dirname $0`
 
-echo "Install requirements via pip"
-pip install --user -r install_requirements.txt
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
 echo "Update apt-get"
 apt-get update
@@ -13,6 +15,10 @@ apt-get install pyaudio -y
 apt-get install pocketsphinx -y
 apt-get install portaudio19-dev -y
 apt-get install ros-kinetic-rosserial-python -y
+
+# pip installs need to run after apt-get installs due to references
+echo "Install requirements via pip"
+pip install --user -r install_requirements.txt
 
 echo "Trigger Setup"
 python $directory/../../Service/Setup.py
