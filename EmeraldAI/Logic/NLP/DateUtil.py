101c889da99bfd63e7f366de99bb0df0c34b60e3
Max <1@porzelt.net>
2019-03-24 23:12:52 +0100

    Update DateUtil.py

--- EmeraldAI/Logic/NLP/DateUtil.py
+++ EmeraldAI/Logic/NLP/DateUtil.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#import re
 import dateparser
 from datetime import datetime
 from dateparser.search import search_dates
 
-
-#from EmeraldAI.Logic.Modules import Global
-#from EmeraldAI.Config.Config import Config
 from EmeraldAI.Logic.Singleton import Singleton
-"""
-if(Config().Get("Database", "NLPDatabaseType").lower() == "sqlite"):
-    from EmeraldAI.Logic.Database.SQlite3 import SQlite3 as db
-elif(Config().Get("Database", "NLPDatabaseType").lower() == "mysql"):
-    from EmeraldAI.Logic.Database.MySQL import MySQL as db
-"""
+
 
 class DateUtil(object):
     __metaclass__ = Singleton
@@ -31,7 +22,7 @@ class DateUtil(object):
 
         now = datetime.now()
         searchResult = search_dates(input)
-        
+
         if searchResult is None:
             return None
 
